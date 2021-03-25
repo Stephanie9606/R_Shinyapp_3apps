2143badb662ad1c2bd2c82662097824a1803898f
Stephanie <stephaniehan628@gmail.com>
2021-03-26 02:27:01 +0800

    server

--- mpg_app/R/mpg_app_stephanie/app.R
+++ mpg_app/R/mpg_app_stephanie/app.R
@@ -4,13 +4,18 @@ library(shiny)
 library(ggplot2)
 
 ui <- fluidPage(
-    varSelectInput("carcol", "X variable", data = mpg, selected = "cty"),
-    varSelectInput("carcol", "Y variable", data = mpg, selected = "hwy"),
-    varSelectInput("carcol", "Color cariable (categorical)", data = mpg, selected = "class")
+    varSelectInput("var1", "X variable", data = mpg, selected = "cty"),
+    varSelectInput("var2", "Y variable", data = mpg, selected = "hwy"),
+    varSelectInput("colc", "Color cariable (categorical)", data = mpg, selected = "class"),
+    plotOutput("plot")
 )
 
 server <- function(input, output) {
- 
+    output$plot <- renderPlot({
+        ggplot(mpg, aes(x = !!input$var1, y = !!input$var2, col = !!input$colc)) +
+            geom_point() +
+            theme_bw()
+    })
 }
 
 shinyApp(ui = ui, server = server)
