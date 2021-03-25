43ee6bd1d1e0a1bdb49b20c4f4be4a6064bf7fa8
Stephanie <stephaniehan628@gmail.com>
2021-03-26 02:36:51 +0800

    ui

--- mtcars_app/R/mtcars_app_stephanie/app.R
+++ mtcars_app/R/mtcars_app_stephanie/app.R
@@ -1,2 +1,21 @@
 # Author: Sihyuan Han
 
+library(shiny)
+library(ggplot2)
+plotchoices <- c("Density Plot", "Histogram", "Frequency Polygon")
+
+
+ui <- fluidPage(
+    varSelectInput("var1", "X variable", data = mtcars),
+    radioButtons("plottype", "Choose a plot type", choices = plotchoices),
+    # plotOutput("plot")
+)
+
+server <- function(input, output) {
+    # output$plot <- renderPlot({
+    #     ggplot(mtcars, aes(x = !!input$var1, y = !!input$var2)) +
+    #         geom_point()
+    # })
+}
+
+shinyApp(ui = ui, server = server)
