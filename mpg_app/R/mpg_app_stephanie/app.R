9207b6d26c7d4400842a380a515f6e6cd6e55dbc
Stephanie <stephaniehan628@gmail.com>
2021-03-26 02:18:19 +0800

    ui

--- mpg_app/R/mpg_app_stephanie/app.R
+++ mpg_app/R/mpg_app_stephanie/app.R
@@ -1,2 +1,16 @@
 # Author: Sihyuan Han
 
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+    varSelectInput("carcol", "X variable", data = mpg, selected = "cty"),
+    varSelectInput("carcol", "Y variable", data = mpg, selected = "hwy"),
+    varSelectInput("carcol", "Color cariable (categorical)", data = mpg, selected = "class")
+)
+
+server <- function(input, output) {
+ 
+}
+
+shinyApp(ui = ui, server = server)
