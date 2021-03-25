e1b83a59ea42c985ec4d55fed3cb6d3e1ab5f973
Stephanie <stephaniehan628@gmail.com>
2021-03-26 02:40:10 +0800

    density plot

--- mtcars_app/R/mtcars_app_stephanie/app.R
+++ mtcars_app/R/mtcars_app_stephanie/app.R
@@ -8,14 +8,15 @@ plotchoices <- c("Density Plot", "Histogram", "Frequency Polygon")
 ui <- fluidPage(
     varSelectInput("var1", "X variable", data = mtcars),
     radioButtons("plottype", "Choose a plot type", choices = plotchoices),
-    # plotOutput("plot")
+    plotOutput("plot")
 )
 
 server <- function(input, output) {
-    # output$plot <- renderPlot({
-    #     ggplot(mtcars, aes(x = !!input$var1, y = !!input$var2)) +
-    #         geom_point()
-    # })
+    output$plot <- renderPlot({
+    ggplot(mtcars, aes(x = !!input$var1)) +
+    geom_density(outline.type = "full") +
+    theme_bw()
+    })
 }
 
 shinyApp(ui = ui, server = server)
