6e910369860bcad4c8f186f0231803318d1c0330
Stephanie <stephaniehan628@gmail.com>
2021-03-27 00:05:22 +0800

    add switch to active

--- mtcars_app/R/mtcars_app_stephanie/app.R
+++ mtcars_app/R/mtcars_app_stephanie/app.R
@@ -4,7 +4,6 @@ library(shiny)
 library(ggplot2)
 plotchoices <- c("Density Plot", "Histogram", "Frequency Polygon")
 
-
 ui <- fluidPage(
     varSelectInput("var1", "X variable", data = mtcars),
     radioButtons("plottype", "Choose a plot type", choices = plotchoices),
@@ -12,10 +11,25 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+    
+    # plotType
+    plotType <- function(x, type) {
+        switch(input$plottype,
+               "Density Plot" = geom_density(x, outline.type = "full"),
+               "Histogram" = geom_histogram(x),
+               "Frequency Polygon" = geom_freqpoly(x))
+    }
+    
+    # reactive ggplot
+    p <- reactive({
+        ggplot(mtcars, aes(x = !!input$var1))
+    })
+    
+    # plot
     output$plot <- renderPlot({
-    ggplot(mtcars, aes(x = !!input$var1)) +
-    geom_density(outline.type = "full") +
-    theme_bw()
+        p() +
+            plotType(input$plotType) +
+            theme_bw()
     })
 }
 
