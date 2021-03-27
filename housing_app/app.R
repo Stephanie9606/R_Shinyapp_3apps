8e830f26b77f575377ce4e2d5b153cc129cb9e2c
Stephanie <stephaniehan628@gmail.com>
2021-03-28 00:41:16 +0800

    if else for diff plot

--- housing_app/app.R
+++ housing_app/app.R
@@ -26,7 +26,7 @@ ui <- fluidPage(
     tabsetPanel(type = "pills",
                 tabPanel("Univariate",
                   varSelectInput("var1", "Variable?", data = estate),
-                  checkboxInput("log1", "Log_Transform?"), 
+                  checkboxInput("log1", "Log_Transform?", value = FALSE), 
                   sliderInput("bins", "Number of Bins?", value = 40, min = 1, max = 100),
                   numericInput("num2", "Null Value", value = 0),
                   verbatimTextOutput("code")
@@ -50,19 +50,55 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  attach(estate)
-  
   output$code <- renderPrint({
-    t.test(estate[[input$var1]], null.value = input$num2)
+    
+   # new df
+   est_newdf <- estate  
+      
+   if(isTRUE(input$log1)){
+      est_newdf$ttvalue <- log(est_newdf[[as.character(input$var1)]])
+    }
+    
+   if(!isTRUE(input$log1)){
+      est_newdf$ttvalue <- est_newdf[[as.character(input$var1)]]
+    }
+    
+   # validate(
+   #   need(is.factor(est_newdf$ttvalue) == TRUE, "Variable is not numeric")
+   #  )
+   
+    tout <- t.test(est_newdf$ttvalue, null.value = input$num2)
+    print(tout)
   })
   
+  # spreadsheet
   output$dynamic <- renderDataTable({
     estate
   })
   
+  # plot
   output$plot <- renderPlot({
-    ggplot(estate, aes(x = !!input$var1)) +
-      geom_histogram(bins = input$bins)
+    
+    # new df
+    est_newdf <- estate  
+    
+    if(isTRUE(input$log1)){
+      est_newdf$ttvalue <- log(est_newdf[[as.character(input$var1)]])
+    }
+    
+    if(!isTRUE(input$log1)){
+      est_newdf$ttvalue <- est_newdf[[as.character(input$var1)]]
+    }
+    
+    # if else numeric var
+    if(is.numeric(est_newdf$ttvalue) == T){
+      ggplot(est_newdf, aes(x = ttvalue)) +
+        geom_histogram(bins = input$bins)
+    } else{
+      ggplot(est_newdf, aes(x = ttvalue)) +
+        geom_bar(bins = input$bins)
+    }
+  
   })
 }
 
