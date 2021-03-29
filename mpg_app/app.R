ce2abccf55761f6eb31e3be13db22a63ba5e97ee
Stephanie <stephaniehan628@gmail.com>
2021-03-29 20:59:06 +0800

    shiny ready

--- mpg_app/app.R
+++ mpg_app/app.R
@@ -6,13 +6,13 @@ library(ggplot2)
 ui <- fluidPage(
     varSelectInput("var1", "X variable", data = mpg, selected = "cty"),
     varSelectInput("var2", "Y variable", data = mpg, selected = "hwy"),
-    varSelectInput("colc", "Color cariable (categorical)", data = mpg, selected = "class"),
+    varSelectInput("colvc", "Color variable (categorical)", data = mpg, selected = "class"),
     plotOutput("plot")
 )
 
 server <- function(input, output) {
     output$plot <- renderPlot({
-        ggplot(mpg, aes(x = !!input$var1, y = !!input$var2, col = !!input$colc)) +
+        ggplot(mpg, aes(x = !!input$var1, y = !!input$var2, col = !!input$colvc)) +
             geom_point() +
             theme_bw()
     })
